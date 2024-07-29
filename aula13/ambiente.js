fd22b6189d29a3dd1bff8b06aa46c067d70f24c8
sergio-ramon <sergioramon.sr98@hotmail.com>
2024-07-29 13:07:28 -0300

    Aula 13

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,1 +1,15 @@
-console.log('Tudo bem?')
+var c = 1;
+
+//Estrutura de repetição com teste lógico no início
+/*
+while(c <= 10) {
+    console.log(`Passo ${c}`);
+    c++;
+}
+*/
+
+//Estrutura de repetição com teste lógico no final
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 10)
